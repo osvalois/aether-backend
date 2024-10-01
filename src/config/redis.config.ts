bbd3b3528c81b04a854efac3dde6bceddec68ff5
Oscar Valois <118651508+osvalois@users.noreply.github.com>
2024-10-01 09:07:52 -0600

    Update redis.config.ts

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -4,13 +4,11 @@ import { RedisOptions } from 'ioredis';
 export default registerAs('redis', (): RedisOptions => ({
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined, // Si usas autenticación
-  db: parseInt(process.env.REDIS_DB, 10) || 0, // Si usas múltiples bases de datos en Redis
+  password: process.env.REDIS_PASSWORD || undefined,
+  db: parseInt(process.env.REDIS_DB, 10) || 0, 
   retryStrategy: (times) => {
-    // Intenta reconectar durante un tiempo específico
-    const delay = Math.min(times * 50, 2000); // Retraso entre reconexiones
-    return delay; // Retorna el tiempo de espera
+    const delay = Math.min(times * 50, 2000);
+    return delay;
   },
-  connectTimeout: 10000, // Timeout de conexión en ms
-  // Otras opciones pueden añadirse aquí si es necesario
+  connectTimeout: 10000,
 }));
